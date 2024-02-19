74bedf34f7dc8a9e06a9a91cbae3e51f120af152
Kav <kevincamilo56@gmail.com>
2024-02-19 08:09:06 -0500

    Tercer Actualizacion

--- bootstrap-5.0.2-examples/modules/history.js
+++ bootstrap-5.0.2-examples/modules/history.js
@@ -9,84 +9,84 @@ const history = express.Router();
 
 //Consultar
 
-history.get("/history/Listin",(req,res)=>{
-    cnx.query("SELECT * FROM history ORDER BY date",(error,datos)=>{
-        
-    try{
-    res.status(200).send(datos);
-    }catch(error){
-        console.log(error);
-    }
-    });
-})//listo
+history.get("/history/Listin", (req, res) => {
+    cnx.query("SELECT * FROM history ORDER BY date", (error, datos) => {
 
-
-history.get("/history/Listin/:id",(req,res)=>{
-    let id = req.params.id;
-    cnx.query("SELECT * FROM history WHERE id = "+id, (error,datos)=>{
-    try{
-    
-        if(datos == ""){
-            res.status(200).send("Don't exist");
-        }else{
+        try {
             res.status(200).send(datos);
+        } catch (error) {
+            console.log(error);
         }
-    }catch(error){
-    
-    console.log(error);
-    }
     });
 })//listo
 
-history.get("/history/deleteid/:id",(req,res)=>{
-        let id = req.params.id;
-        cnx.query("DELETE FROM history WHERE id = "+id, (error,datos)=>{
-           
-        try{
-        res.status(200).send("Success delete");
-        }catch(error){
-        console.log(error);
-        }
-        });
-        })//listo
-        
 
-        history.get("/history/insert/:description/:date/:note/:idPerson",(req,res)=>{
-            let description = req.params.description
-            let date = req.params.date
-            let note = req.params.note
-            let idPerson = req.params.idPerson
-    
-            cnx.query("INSERT INTO history VALUES ('','"+description+"','"+date+"','"+note+"','"+idPerson+"') ", (error,datos)=>{
-            try{
+history.get("/history/Listin/:id", (req, res) => {
+    let id = req.params.id;
+    cnx.query("SELECT * FROM history WHERE id = " + id, (error, datos) => {
+        try {
+
+            if (datos == "") {
+                res.status(200).send("Don't exist");
+            } else {
+                res.status(200).send(datos);
+            }
+        } catch (error) {
+
+            console.log(error);
+        }
+    });
+})//listo
+
+history.get("/history/deleteid/:id", (req, res) => {
+    let id = req.params.id;
+    cnx.query("DELETE FROM history WHERE id = " + id, (error, datos) => {
+
+        try {
+            res.status(200).send("Success delete");
+        } catch (error) {
+            console.log(error);
+        }
+    });
+})//listo
+
+
+history.get("/history/insert/:description/:date/:note/:idPerson", (req, res) => {
+    let description = req.params.description
+    let date = req.params.date
+    let note = req.params.note
+    let idPerson = req.params.idPerson
+
+    cnx.query("INSERT INTO history VALUES ('','" + description + "','" + date + "','" + note + "','" + idPerson + "') ", (error, datos) => {
+        try {
             res.status(200).send("Success insert");
-            }catch(error){
+        } catch (error) {
             console.log(error);
 
-         
-            }
-            
-             
-            });
-            })
+
+        }
 
 
-            history.get("/history/update/:id/:description/:date/:note/:idPerson",(req,res)=>{
-                let id = req.params.id;
-                let description = req.params.description
-                let date =req.params.date
-                let note = req.params.note
-                let idPerson = req.params.idPerson
-                
-                 cnx.query("UPDATE history SET description = '"+description+"', date = '"+date+"' , note = '"+note+"', id_people = '"+idPerson+"' WHERE id= " +id, (error,datos)=>{
-        
-                 try{
-                     res.status(200).send("Success update");
-                    }catch(error){
-                        console.log(error);
-                            }
-                            
-                            });
-                            })
+    });
+})
+
+
+history.get("/history/update/:id/:description/:date/:note/:idPerson", (req, res) => {
+    let id = req.params.id;
+    let description = req.params.description
+    let date = req.params.date
+    let note = req.params.note
+    let idPerson = req.params.idPerson
+
+    cnx.query("UPDATE history SET description = '" + description + "', date = '" + date + "' , note = '" + note + "', id_people = '" + idPerson + "' WHERE id= " + id, (error, datos) => {
+
+        try {
+            res.status(200).send("Success update");
+        } catch (error) {
+            console.log(error);
+        }
+
+    });
+})
 
 module.exports = history;
--- bootstrap-5.0.2-examples/modules/people.js
+++ bootstrap-5.0.2-examples/modules/people.js
@@ -8,86 +8,90 @@ const people = express.Router();
 /* Desarrollo de CRUD */
 
 
-//Consultar
+ 
 
-people.get("/people/Listin",(req,res)=>{
-    cnx.query("SELECT * FROM people ORDER BY id",(error,datos)=>{
-        
-    try{
-    res.status(200).send(datos);
-    }catch(error){
-        console.log(error);
-    }
-    });
-})//listo
+people.get("/people/Listin", (req, res) => {
+    cnx.query("SELECT * FROM people ORDER BY id", (error, datos) => {
 
-
-people.get("/people/Listin/:id",(req,res)=>{
-    let id = req.params.id;
-    cnx.query("SELECT * FROM people WHERE id = "+id, (error,datos)=>{
-    try{
-    
-        if(datos == ""){
-            res.status(200).send("Don't exist");
-        }else{
+        try {
             res.status(200).send(datos);
+        } catch (error) {
+            console.log(error);
         }
-    }catch(error){
-    
-    console.log(error);
-    }
     });
-})//listo
+})
 
-    people.all("/people/deleteid/:id",(req,res)=>{
-        let id = req.params.id;
-        cnx.query("DELETE FROM people WHERE id = "+id, (error,datos)=>{
-           
-        try{
-        res.status(200).send("Success delete");
-        }catch(error){
-        console.log(error);
+
+people.get("/people/Listin/:id", (req, res) => {
+    let id = req.params.id;
+    cnx.query("SELECT * FROM people WHERE id = " + id, (error, datos) => {
+        try {
+
+            if (datos == "") {
+                res.status(200).send("Don't exist");
+            } else {
+                res.status(200).send(datos);
+            }
+        } catch (error) {
+
+            console.log(error);
         }
-        });
-        })//listo
-        
+    });
+})
 
-        people.all("/people/insert/:name/:lastname/:nickname/:email/:type",(req,res)=>{
-            let name = req.params.name
-            let lastname = req.params.lastname
-            let nickname = req.params.nickname
-            let email = req.params.email  
-            let type = req.params.type
-            cnx.query("INSERT INTO people VALUES ('','"+name+"','"+lastname+"','"+nickname+"','"+email+"', "+type+")", (error,datos)=>{
-            try{
+people.all("/people/deleteid/:id", (req, res) => {
+    let id = req.params.id;
+    cnx.query("DELETE FROM people WHERE id = " + id, (error, datos) => {
+
+        try {
+            res.status(200).send("Success delete");
+        } catch (error) {
+            console.log(error);
+        }
+    });
+})
+
+
+people.all("/people/insert/:name/:lastname/:nickname/:email/:type", (req, res) => {
+    let name = req.params.name
+    let lastname = req.params.lastname
+    let nickname = req.params.nickname
+    let email = req.params.email
+    let type = req.params.type
+    cnx.query("INSERT INTO people VALUES ('','" + name + "','" + lastname + "','" + nickname + "','" + email + "', " + type + ")", (error, datos) => {
+        try {
             res.status(200).send("Success insert");
-            }catch(error){
+        } catch (error) {
             console.log(error);
 
-         
-            }
-            
-             
-            });
+
+        }
+
+
+    });
+})
+
+people.all("/people/update/:id/:name/:lastname/:nickname/:email/:type", (req, res) => {
+    let id = req.params.id;
+    let name = req.params.name
+    let lastname = req.params.lastname
+    let nickname = req.params.nickname
+    let email = req.params.email
+    let type = req.params.type
+    cnx.query("UPDATE people SET name = '" + name + "', lastname = '" + lastname + "' , nickname = '" + nickname + "', email = '" + email + "', type = '" + type + "' WHERE id= " + id, (error, datos) => {
+
+        try {
+            res.status(200).send("Success update");
+        } catch (error) {
+            res.status(400).send({
+                codigo: "error",
+                mensaje: "No es posible ejecutar la consulta, revise la informacion",
+                id: error.code,
+                mensaje: error.message
             })
+        }
 
-
-        people.all("/people/update/:id/:name/:lastname/:nickname/:email/:type",(req,res)=>{
-                let id = req.params.id;
-                let name = req.params.name
-                let lastname = req.params.lastname
-                let nickname = req.params.nickname
-                let email = req.params.email  
-                let type = req.params.type  
-                 cnx.query("UPDATE people SET name = '"+name+"', lastname = '"+lastname+"' , nickname = '"+nickname+"', email = '"+email+"', type = '"+type+"' WHERE id= " +id, (error,datos)=>{
-        
-                 try{
-                     res.status(200).send("Success update");
-                    }catch(error){
-                        console.log(error);
-                            }
-                            
-                            });
-                            })
+    });
+})
 
 module.exports = people;
